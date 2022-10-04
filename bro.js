66acc18af2bdb649c61e737581dd1dbdee6b225f
Amrakh9 <emrahamiraslanov@gmail.com>
2022-10-04 09:32:39 +0400

    ff

---
+++ bro.js
@@ -0,0 +1,89 @@
+// var a,b,c,d,e,f;
+// a=Math.floor(Math.random()*100);
+// b=Math.floor(Math.random()*100);
+// c=Math.floor(Math.random()*100);
+// d=Math.floor(Math.random()*100);
+// e=Math.floor(Math.random()*100);
+// f=Math.floor(Math.random()*100);
+// document.write("<table border=1 width=500>");
+// document.write("<tr>");
+// document.write("<td>" + a + "</td>" + "<td>" + b + "</td>" + "<td>" + c + "</td>");
+// document.write("</tr>");
+// document.write("<tr>");
+// document.write("<td>" + d + "</td>" + "<td>" + e + "</td>" + "<td>" + f + "</td>");
+// document.write("</tr>");
+// document.write("</table>");
+
+
+
+
+
+// var a;
+// a = prompt("ededi daxil edin :");
+// if(a > 10) document.write("10 dan boyukdur");
+// else document.write("10 dan kicikdir");
+
+
+
+
+// function funcksiya(a,b){
+//     var c=a+b;
+//     document.write(c);
+// }
+// var e,g,h;
+// e = prompt("birinci eded:");
+// g = prompt("ikinci eded:");
+// h = funcksiya(e,g);
+
+
+
+// function funksiya() {
+//     document.write("Ok<br>");
+// }
+// function barbara() {
+//     document.write("<p>Barbara</p>");
+// }
+
+
+
+// function toplama(a, b) {
+//     a = prompt("birinci eded:");
+//     b = prompt("ikinci eded:");
+//     var c = a + b;
+//     document.write(c);
+// }
+// function kokalti(a, b) {
+//     a = prompt("birinci eded:");
+//     b = prompt("ikinci eded:");
+//     var c = a + b;
+//     document.write(Math.sqrt(c,2));
+// }
+// function kvadrat(a, b) {
+//     a = prompt("birinci eded:");
+//     b = prompt("ikinci eded:");
+//     var c = (a + b);
+//     document.write(Math.pow(c, 2));
+// }
+
+
+// document.getElementById("yazi").innerHTML="Ikinci yazi";
+
+// document.getElementById("barbara").innerHTML
+
+
+
+
+// function funksiya() {
+//     var word = document.getElementById("word").value;
+//     if(word =="apple"){
+//         document.getElementById("translation").value="alma";
+//     }
+//     if(word =="orange"){
+//         document.getElementById("translation").value="portagal";
+//     }
+//     if(word =="banana"){
+//         document.getElementById("translation").value="banan";
+//     }
+// }
+
+
